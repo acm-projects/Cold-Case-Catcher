f185dff155b6a26bbadb35b3d32bcd3418dd53cc
Alexw360 <alexw360@verizon.net>
2022-02-20 19:13:28 -0600

    created a basic react app with a homepage

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Cold Case Catcher</h1>
+      <h2>Homepage with search bar and preview of cases</h2>
+    </div>
+  );
+}
+
+export default App;
