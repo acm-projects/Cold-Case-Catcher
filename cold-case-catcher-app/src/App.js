2551bd360c0ef2fddf2309c0f9ee914a21ddf06d
salmanh123 <salmanh084@gmail.com>
2022-03-22 01:46:37 -0500

    Created toggler component

--- cold-case-catcher-app/src/App.js
+++ cold-case-catcher-app/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import { db } from './firebase';
-import { collection, doc, getDocs, addDoc, updateDoc, deleteDoc } from 'firebase/firestore'
-i//mport {manslaughter} from './data/manslaughter.js'
+import { collection, doc, query, where, getDocs,  addDoc, updateDoc, deleteDoc } from 'firebase/firestore'
+//import {manslaughter} from './data/manslaughter.js'
 //import {murder} from './data/murder'
 function App() {
 
@@ -11,12 +11,21 @@ function App() {
   const [newTitle, setTitle] = useState("")
   const [newDate, setDate] = useState("")
   const [newStory, setStory] = useState("")
+  // Toggler States
+  const [mansLState, setManslState] = useState(false)
+  const [hitRunState, setHitRunState] = useState(false)
+  const [rmState, setRMState] = useState(false)
+  const [murderState, setMState] = useState(false)
+  const [mpState, setMPState] = useState(false)
+  const [sdState, setSDState] = useState(false)
+
   // Collection reference
   const casesCollectionRef = collection(db, "cases") // establish connection to db & collection
 
   // Retrieves cases with getDocs & our collection reference
-  const getCases = async () => {
-    const data = await getDocs(casesCollectionRef)
+  const getCases = async (crime = "Hit and Run") => {
+    const q = query(casesCollectionRef, where("Offense","==",`${crime}`))
+    const data = await getDocs(q)
     setCases(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
   }
 
@@ -26,6 +35,7 @@ function App() {
     await addDoc(casesCollectionRef, { date: newDate, story: newStory, title: newTitle })
 
   }
+  // pass object and it adds to the referred database collection
   const addCase = async (theCase) => {
     await addDoc(casesCollectionRef, theCase)
 
@@ -48,12 +58,22 @@ function App() {
 
   // Called eachtime page is rendered
   useEffect(() => {
-    getCases()
+    //getCases("Suspicious Death")
     
   }, [])
 
   // to pass arguments to function called with onClick it needs to be in an
   // arrow function first, else you dont such as with createUser
+  const dispCases = async (crime) =>{
+    const q = query(casesCollectionRef, where("Offense","==",`${crime}`))
+    const data = await getDocs(q)
+    let returnedCases = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+    setCases( [...cases, ...returnedCases])
+    console.log(cases)
+  }
+  const filterCases = async(crime) =>{
+    setCases(cases.filter((theCase)=>theCase['Offense'] !== crime))
+  }
 
   return (
     // we create input fields that concurrently update the state variables
@@ -62,19 +82,54 @@ function App() {
       <h1>Hello</h1>
 
       <div>
-        <input type="text" placeholder='title' onChange={(e) => { setTitle(e.target.value) }}></input>
+        {/* <input type="text" placeholder='title' onChange={(e) => { setTitle(e.target.value) }}></input>
         <input type="text" placeholder='date' onChange={(e) => { setDate(e.target.value) }}></input>
         <input type="text" placeholder='story' onChange={(e) => { setStory(e.target.value) }}></input>
-        <button onClick={createUser}>Create Story</button>
+        <button onClick={createUser}>Create Story</button> */}
+        <button onClick={()=> {
+          if(mansLState === false){ setManslState(true); dispCases("Manslaughter/ Non-Negligent")}
+          else { setManslState(false); filterCases("Manslaughter/ Non-Negligent")}
+        }}> Manslaughter </button>
+
+        <button onClick={()=> {
+          if(hitRunState === false){
+            setHitRunState(true)
+            dispCases("Hit and Run")
+          }
+          else{
+            setHitRunState(false)
+            filterCases("Hit and Run")
+          }
+        }}>Hit & Run</button>
+
+        <button onClick={()=> {
+          if(rmState === false){ setRMState(true); dispCases("Rape/Murder")}
+          else { setRMState(false); filterCases("Rape/Murder")}
+        }}>Rape & Murder</button>
+
+        <button onClick={()=> {
+          if(mpState === false){ setMPState(true); dispCases("Missing Person (Presumed Murdered)")}
+          else { setMPState(false); filterCases("Missing Person (Presumed Murdered)")}
+        }}>Missing Person</button>
+
+        <button onClick={()=> {
+          if(sdState === false){ setSDState(true); dispCases("Suspicious Death")}
+          else { setSDState(false); filterCases("Suspicious Death")}
+        }}>Suspicious Death </button>
+
+        <button onClick={()=> {
+          if(murderState === false){ setMState(true); dispCases("Murder")}
+          else { setMState(false); filterCases("Murder")}
+        }}>Murder</button>
       </div>
 
 
       {cases.map((newCase) => {
         return (
           <div>
-            <h1>Name: {newCase.title}</h1>
-            <h1>Story: {newCase.story}</h1>
-            <h1>Date: {newCase.date}</h1>
+            <h1>Name: {newCase['Name']}</h1>
+            <h1>Story: {newCase['Offense']}</h1>
+            <h1>Date: {newCase['Incident Location']}</h1>
             <button onClick={() => { updateDate(newCase.id, newCase.date) }}>Adjust date</button>
             <button onClick={() => { deleteUser(newCase.id) }}>Delete data </button>
 
