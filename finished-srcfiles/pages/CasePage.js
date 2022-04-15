f0b0391ed74012912fb4ed85199eb941ece4fb42
aditi-mungale <89321986+aditi-mungale@users.noreply.github.com>
2022-04-15 14:01:54 -0500

    Create CasePage.js

---
+++ finished-srcfiles/pages/CasePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Components/Header'
+import CaseDetail from '../Components/CaseDetail'
+
+function Case() {
+  return (
+    <>
+    <Header/>
+    <CaseDetail/>
+    </>
+  )
+}
+
+export default Case
